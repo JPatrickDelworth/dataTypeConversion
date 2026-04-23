10d2975deee3d6415dc39b85e1104990cdd47483
J Patrick Delworth <jpdelworth@gmail.com>
2026-04-23 01:54:45 -0500

    First altered each prompt to either fix type conversion errors or provide alternative conversions. I then provided my own examples of explicit and implicit conversion. Every time a variable changed data types in the original code and my code, I printed its data type to console using console.log(typeof()).

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,72 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
-
+// This first example doesn't need to change as it converts the string "5" to a number using the subtraction operator. So I will have it print normally and then show a version where the opposite occurs.
 let result = "5" - 2;
+console.log(" ");
 console.log("The result is: " + result);
+console.log("Data Type of result: " + typeof(result));
+result = "5" + 2; // I changed the - to a + which reverses the data type conversion. Initially, the - converted the string "5" into a number and then subrated 3. By changing to + the number 2 is converted into a string and concatenated onto string "5".
+console.log(" ");
+console.log("The result is now: " + result)
+console.log("Data Type of result: " + typeof(result));
 
-let isValid = Boolean("false");
-if (isValid) {
-    console.log("This is valid!");
+console.log(" ");
+let isValid = "false"; //Even though the string is the word "false" Boolean() will change it to true since strings are truthy.
+console.log("The Data Type of isValid is: " + typeof(isValid));
+isValid = Boolean(isValid);
+console.log("The Data Type of isValid is now: " + typeof(isValid));
+if(isValid)
+{
+  console.log("This is valid!");
+}
+else 
+{
+  console.log("This is false!");
 }
 
+console.log(" ");
+isValid = NaN; // Only six values can be changed to false using Boolean(). Strings are not one of them unless empty. I changed "false" to NaN, one of the 6 falsy values.
+console.log("The Data Type of isValid is: " + isValid);
+isValid = Boolean(isValid);
+console.log("The Data Type of isValid is now: " + typeof(isValid));
+if (isValid) {
+    console.log("This is valid!");
+    
+}
+else {
+  console.log("This is invalid!"); // Added an else statement so the console mentions when the Boolean is false.
+}
+
+console.log(" ");
 let age = "25";
-let totalAge = age + 5;
+console.log("The Data Type of age is: " + typeof(age));
+let totalAge = Number(age) + 5; //By converting "25" to a number, 25 and 5 add correctly instead of the 5 being concatenated onto the end of the string "25"
 console.log("Total Age: " + totalAge);
+console.log("The Data Type of totalAge is: " + typeof(totalAge));
+
+
+//My Examples
+console.log(" ");
+console.log(" ");
+
+console.log("And now for my personal examples.")
+console.log(" ");
+console.log("Explicit Conversion:")
+
+let empty = ""; // empty here contains an empty string. It is considered a string but when Boolean() is used on it, it will become the boolean value false.
+console.log("This variable, empty (" + empty + "), is a " + typeof(empty) + "."); // Here I print the value inside empty and print its data type
+console.log("I will now use Boolean() on empty, converting it into a boolean value.");
+empty = Boolean(empty); // Empty is being converted explicitly into a Boolean value.
+console.log("Now the variable empty (" + empty + ") is a " + typeof(empty) + "."); // Here I print the new value of empty and its data type.
+
+console.log(" ");
+console.log(" ");
+console.log("Implicit Conversion:");
+
+let string = "606"; // string contains the word "606"
+console.log("This variable, string (" + string + "), is a " + typeof(string) + "."); // Here I print the value of string and the data type of its value. 
+console.log("Without using any intentional conversion I'm going to perform some math on this string: string = string / 42");
+string = string / 42; // Using division will transform the string "606" into a number and then divide it by 42;
+console.log("What value is now in string? And what is its data type?")
+console.log("Value: " + string + " Data Type: "  + typeof(string)); // Here I print the new value of string which is the reult of the division I performed. I also print its data type which is now a Number.
